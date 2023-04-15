3dda2c0fd4314e20e1502d77edaf73d594baf535
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2023-04-15 11:39:50 +0530

    admin-portal

---
+++ frontend/admin.js
@@ -0,0 +1,33 @@
+const myForm = document.getElementById("myForm");
+const title = document.getElementById("title");
+const image = document.getElementById("image");
+const price = document.getElementById("price");
+const category = document.getElementById("category");
+const description = document.getElementById("description");
+// let submit = document.getElementById('submit');
+
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let obj = {
+    title: title.value,
+    image: image.value,
+    price: price.value,
+    category: category.value,
+    description: description.value,
+  };
+
+  fetch("http://localhost:8080/products", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(obj),
+  });
+
+  title.value = "" ;
+  image.value = "" ;
+  price.value = "" ;
+  category.value = "" ;
+  description.value = "" ;
+});
